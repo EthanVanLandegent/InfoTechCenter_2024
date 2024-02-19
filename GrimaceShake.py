668f16248b6a4eb32cce12146d2f4497e2c4c305
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-02-19 14:19:00 -0500

    Add GasLevelAlert - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -16,5 +16,13 @@ def listOfFasStations():
      gasStationsNearby = random.choice(gasStation)
      return gasStationsNearby
 
-print(gasLevelGauge())
-print(listOfFasStations())
+#Function will call the gasLevelGauge to determine our gas level and then find a close gas station
+# by calling the listOfGasStations function if we are on Low or Quarter Tank
+def gasLevelAlert():
+     milesToGasStationsLow = random.uniform(1, 25)
+     milesToGasStationsQuarterTank = random.uniform(25.1, 50)
+     #gasLevelGauge = gasLevelGauge()
+     print(milesToGasStationsLow)
+     print(milesToGasStationsQuarterTank)
+
+gasLevelAlert()
